60561847b5b73059286d4b5773b3df9824d9da9b
hert0t <76783942+hert0t@users.noreply.github.com>
2023-05-04 07:25:04 +0700

    Update and rename spins.js to spinx.js

--- assets/js/spinx.js
+++ assets/js/spinx.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$("#form").on( "submit", function( event ) {
+		$("#btnspin").attr("disabled", true);
 		$("#btnspin").html(
 		`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" disabled="true"></span>`
 		);
